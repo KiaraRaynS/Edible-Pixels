9a935711ee4df7db9164474ae3d91f2847006b28
KiaraRaynS <sherman.rayn@outlook.com>
2016-08-30 20:50:24 -0400

    Catuccino page and art added

--- appcatcafe/views.py
+++ appcatcafe/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'indexview.html'
+
+
+# Drinks
+class CatuccinoView(TemplateView):
+    template_name = 'catuccinoview.html'
--- catcafe/urls.py
+++ catcafe/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from appcatcafe.views import IndexView
+# Drinks
+from appcatcafe.views import CatuccinoView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='indexview'),
+    url(r'^drinks/catuccino/$', CatuccinoView.as_view(), name='catuccinoview'),
 ]
